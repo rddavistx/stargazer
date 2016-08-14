95092895807d6ccaadeebb8c4db6390d040aece2
Richard Davis <rddavistx@hotmail.com>
2016-08-14 14:33:49 -0500

    stargazer homework

--- js/stargazer.js
+++ js/stargazer.js
@@ -1,5 +1,29 @@
-function search(name) {
-  var api = 'http://www.strudel.org.uk/lookUP/json/?name='+name;
-  // requires "jsonp" datatype.
-  
+$(document).ready(function () {
+  $('#search').on('submit', function(e) {
+    e.preventDefault(e);
+    var name = $('#search-name').val();
+
+    $.ajax({
+      url: 'http://www.strudel.org.uk/lookUP/json/?name='+name,
+      dataType: "jsonp",
+      jsonpCallback: "results"
+    })
+    .done(function(data) {
+      console.log(data);
+    })
+    .fail(function() {
+      alert('error');
+    })
+
+
+})
+})
+
+
+function results(json){
+  if (json.image) {
+    $('#preview').html('<img src="http:' + json.image.src + '">')
+} else {
+  alert("Star not found")
+}
 }
